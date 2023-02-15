6cc972ba7b8e30282ea05adb600f2f43d90d48b1
Sugyeong <onmay16@gmail.com>
2023-02-14 21:32:51 -0600

    MOMO-23 add components and utils folders

---
+++ src/utils/Api.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Api = () => {
+    return (
+        <View>
+            <Text>Api</Text>
+        </View>
+    )
+}
+
+export default Api
