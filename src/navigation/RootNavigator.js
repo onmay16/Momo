db63d6747e1a09d5554fe9bfc6d5bdb0818ffa79
JDhyeok <jdhyeok97@gmail.com>
2023-02-16 21:50:41 +0900

    chore: Add tab navigation bar

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -4,16 +4,35 @@ import { createStackNavigator } from '@react-navigation/stack'
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import LoginScreen from '../screens/LoginScreen'
+import MainScreen from '../screens/MainScreen'
+import MomoScreen from '../screens/MomoScreen';
 
 const AuthStack = createStackNavigator();
 const MainScreenTab = createBottomTabNavigator();
 
+const isAuthUser = false;
+
+const TabComponent = () => {
+  return (
+    <MainScreenTab.Navigator>
+      <MainScreenTab.Screen name="Main" component={MainScreen} />
+      <MainScreenTab.Screen name="Momo" component={MomoScreen} />
+    </MainScreenTab.Navigator>
+  )
+}
+
 export const RootNavigator = () => {
   return (
     <AuthStack.Navigator
-      screenOptions={{headerShown:false}}
+      screenOptions={{ headerShown: false }}
     >
-            <AuthStack.Screen name="Login" component={LoginScreen}/>
-        </AuthStack.Navigator>
-    )
+      {isAuthUser ? (
+        <AuthStack.Screen name="Main" component={TabComponent} />
+      ) : (
+        <AuthStack.Screen name="Login" component={LoginScreen} />
+      )
+      }
+
+    </AuthStack.Navigator>
+  )
 }
