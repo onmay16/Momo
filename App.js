9d94e57c2ac46bb5490ed64e9673aefc3a34ddd4
Sugyeong <onmay16@gmail.com>
2023-02-18 15:14:04 -0600

    MOMO-21 chore: render loadingscreen in App.js

--- App.js
+++ App.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 import { NavigationContainer } from '@react-navigation/native';
 import { RootNavigator } from './src/navigation/RootNavigator';
 
+import LoadingScreen from './src/screens/LoadingScreen';
+
 const App = () => {
-  return(
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false);
+    }, 3000);
+  }, []);
+
+  return loading ? (<LoadingScreen/>) : (
     <NavigationContainer>
       <RootNavigator/>
     </NavigationContainer>
-  )
-}
+  );
+};
 
 export default App;
