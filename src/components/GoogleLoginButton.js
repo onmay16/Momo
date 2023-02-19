da8eb2df1970e9beca73f222cd88fe5eefeef88f
Sugyeong <onmay16@gmail.com>
2023-02-18 23:08:51 -0600

    MOMO-25 test: redux user state simple test

--- src/components/GoogleLoginButton.js
+++ src/components/GoogleLoginButton.js
@@ -1,18 +1,14 @@
 import React from 'react';
-import { View, TouchableOpacity, Image } from 'react-native';
+import {View, TouchableOpacity, Image} from 'react-native';
 
 const GoogleLoginButton = () => {
   return (
     <View>
-      <TouchableOpacity
-        onPress={() => alert('Google Login')}
-      >
-        <Image
-          source={require('../assets/images/Google.png')}
-        />
+      <TouchableOpacity onPress={() => alert('Google Login')}>
+        <Image source={require('../assets/images/google.png')} />
       </TouchableOpacity>
     </View>
-  )
-}
+  );
+};
 
 export default GoogleLoginButton;
