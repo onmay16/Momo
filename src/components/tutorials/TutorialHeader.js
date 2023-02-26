88a98d51ed89040e84f324b6e585bd36f3d416bf
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-02-26 21:03:56 +0900

    feat: Add Buttons TouchableOpacity
    - Header / Bottom 버튼에 TouchableOpacity 적용

--- src/components/tutorials/TutorialHeader.js
+++ src/components/tutorials/TutorialHeader.js
@@ -1,5 +1,5 @@
 import { useEffect } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 const TutorialHeader = props => {
     useEffect(() => {
@@ -8,8 +8,12 @@ const TutorialHeader = props => {
 
     return (
     <View style={{flex: 1, margin:15, flexDirection:"row", justifyContent:"space-between"}}>
-        <Text style={{color:props.textColor}}>이전으로</Text>
-        <Text style={{color:props.textColor}}>건너뛰기</Text>
+        <TouchableOpacity onPress={() => alert('이전으로')}>
+            <Text style={{color:props.textColor}}>이전으로</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress={() => alert('건너뛰기')}>
+            <Text style={{color:props.textColor}}>건너뛰기</Text>    
+        </TouchableOpacity>
     </View>
     )
 }
