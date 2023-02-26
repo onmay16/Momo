fecf1498c6af52bd91d9ec98f5e634915a507e6c
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-02-26 21:03:56 +0900

    feat: Add Buttons TouchableOpacity
    - Header / Bottom 버튼에 TouchableOpacity 적용

--- src/components/tutorials/BottomButtonComponent.js
+++ src/components/tutorials/BottomButtonComponent.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, TouchableOpacity } from 'react-native';
 
 const BottomButtonComponent = props => {
     return (
-    <View style={{flex: 1, backgroundColor: "#3CE3AC", justifyContent:"center", opacity:props.opacity}}>
-        <Text style={{textAlign:"center", color:"black", fontSize:20, fontWeight: 700}}>다음</Text>
-    </View>
+        <TouchableOpacity style={{flex: 1, backgroundColor: "#3CE3AC", justifyContent:"center", opacity:props.opacity}} onPress={() => alert("Bottom Button OnClick!")}>
+            <Text style={{textAlign:"center", color:"black", fontSize:20, fontWeight: 700}}>다음</Text>
+        </TouchableOpacity>
     )
 }
 
