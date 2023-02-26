76a1399775356667ef45200b57f64f9b31b1c897
corea2244 <59365194+corea2244@users.noreply.github.com>
2023-02-26 20:03:21 +0900

    feat: Add Tutorial Header Component
    작업:
    - [이전으로] / [건너뛰기] 버튼 위치 설정
    - 배경색 변경에 따른 텍스트 색상 변경
    
    todo:
    - touchableOpacity 로 변경
    - 배경 이미지가 position absolute 이므로 터치 동작 여부 체크 필요
    - 튜토리얼 단계에 따라서 버튼 표시 여부 변경

---
+++ src/components/tutorials/TutorialHeader.js
@@ -0,0 +1,17 @@
+import { useEffect } from 'react';
+import { View, Text } from 'react-native';
+
+const TutorialHeader = props => {
+    useEffect(() => {
+        console.log("헤더창 로드")
+    }, []);
+
+    return (
+    <View style={{margin:15, flexDirection:"row", justifyContent:"space-between"}}>
+        <Text style={{color:props.textColor}}>이전으로</Text>
+        <Text style={{color:props.textColor}}>건너뛰기</Text>
+    </View>
+    )
+}
+
+export default TutorialHeader;
--- src/screens/tutorials/MainTutorialScreen.js
+++ src/screens/tutorials/MainTutorialScreen.js
@@ -1,13 +1,16 @@
 import { useEffect, useState } from 'react';
-import {View} from 'react-native';
+import {View, SafeAreaView} from 'react-native';
 import BackgroundImgComponent from '../../components/tutorials/BackgroundImgComponent';
+import TutorialHeader from '../../components/tutorials/TutorialHeader';
 
 const MainTutorialScreen = () => {
     const [isChangeBackground, setisChangeBackground] = useState(false);
+    const [tutorialTextColor, settutorialTextColor] = useState("black");
 
     useEffect(() => {
         setTimeout(() => {
             setisChangeBackground(true);
+            settutorialTextColor("white");
             console.log("Main Screen");
         }, 3000);
     }, []);
@@ -15,6 +18,9 @@ const MainTutorialScreen = () => {
     return(
         <View style={{flex:1}}>
             <BackgroundImgComponent isChangeBackground={isChangeBackground}/>
+            <SafeAreaView style={{flex:1}}>
+                <TutorialHeader textColor={tutorialTextColor}/>
+            </SafeAreaView>
         </View>
     );
 }
