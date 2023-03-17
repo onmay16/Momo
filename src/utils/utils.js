5230c61bfcad392a375d987a9d4b138c00534fc2
JDhyeok <jdhyeok97@gmail.com>
2023-03-17 21:52:28 +0900

    MOMO-27 feat: add asyncstorage

---
+++ src/utils/utils.js
@@ -0,0 +1,22 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+const storeAuthToken = async (value) => {
+  try {
+    await AsyncStorage.setItem('@authToken', value);
+  } catch (e) {
+    console.error(error);
+  }
+};
+
+const getAuthToken = async (callBack) => {
+  try {
+    await AsyncStorage.getItem('@authToken').then(value=>{
+      callBack(value)
+    });
+    
+  } catch (e) {
+    console.log(e);
+  }
+};
+
+export {storeAuthToken, getAuthToken};
