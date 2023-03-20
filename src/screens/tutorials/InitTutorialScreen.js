8d796ee1b8ae7431e2f0ed4b77c72d377a5f5ebe
KangHyi Lee <corea22222@gmail.com>
2023-03-20 22:40:37 +0900

    MOMO-28 feat: apply redux state (5)
    - Step: Enum 으로 구현
    - Step 1 TimePicker 까지 화면 전환

--- src/screens/tutorials/InitTutorialScreen.js
+++ src/screens/tutorials/InitTutorialScreen.js
@@ -15,7 +15,6 @@ import { useDispatch, useSelector } from 'react-redux';
 import TutorialMomoImage from '../../assets/images/TutorialMomo.png';
 
 import { 
-  useBackgroundImg,
   setStep,
 } from '../../redux/reducerSlices/tutorialSlice';
 
