706c239b08d6a580b8155af070969968854cc0a1
onmay16 <onmay16@gmail.com>
2023-03-20 22:29:48 -0500

    MOMO-42: add new UI components

---
+++ src/components/CustomComponent/Category.js
@@ -0,0 +1,33 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import PretendardedText from './PretendardedText';
+
+export const Category = (props) => {
+  return (
+    <View style={styles.container}>
+      <PretendardedText style={customSytles(props.name).textColor}>{props.name}</PretendardedText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    width: 52,
+    height: 32,
+    borderRadius: 1000,
+    backgroundColor: 'white',
+    borderWidth: 1.5,
+    borderColor: '#EEEEEE',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+const customSytles = (name) => StyleSheet.create({
+  textColor: {
+    fontWeight: 900,
+    fontSize: 16,
+    color: name === '건강' ? '#3CE3AC' : (name === '성장' ? '#FF6056' : '#6184FF'),
+  },
+});
