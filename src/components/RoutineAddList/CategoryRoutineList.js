5f24b6231473cbac5a372f34b97d5348d22dc5a2
onmay16 <onmay16@gmail.com>
2023-03-20 22:29:48 -0500

    MOMO-42: add new UI components

---
+++ src/components/RoutineAddList/CategoryRoutineList.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import { Category } from '../CustomComponent/Category';
+
+export const CategoryRoutineList = () => {
+  return (
+    <View>
+      <Category name="건강"/>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
