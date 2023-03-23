c3e0d57715b26bdc328def6758f4e1cff3a69bef
JDhyeok <jdhyeok97@gmail.com>
2023-03-23 23:59:01 +0900

    MOMO-18 add background and comming soon image

--- src/screens/ClosetScreen.js
+++ src/screens/ClosetScreen.js
@@ -1,14 +1,31 @@
-import { Text, View, StyleSheet } from 'react-native';
+import {View, StyleSheet, Dimensions, Image} from 'react-native';
 import React from 'react';
 
+import Background from '../assets/images/closet_background.png'
+import CommingSoonImage from '../assets/images/wip.svg';
+
+
 const ClosetScreen = () => {
-    return (
-        <View>
-            <Text>ClosetScreen</Text>
-        </View>
-    );
+  var {height, width} = Dimensions.get('window');
+  return (
+    <View style={styles.container}>
+      <Image source={Background} style={{height: height, width: width, resizeMode: 'stretch'}}/>
+      <CommingSoonImage style={styles.coomingsoonContainer}/>
+    </View>
+  );
 };
 
-export default ClosetScreen;
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    height: '100%',
+    width: '100%'
+  },
+  coomingsoonContainer:{
+    position:'absolute'
+  }
+});
 
-const styles = StyleSheet.create({});
+export default ClosetScreen;
