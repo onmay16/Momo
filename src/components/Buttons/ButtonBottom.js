09dae36a202285c0b6ec29dc0ac4a194ee77f132
onmay16 <onmay16@gmail.com>
2023-03-23 23:15:51 -0500

    MOMO-42: add button component

---
+++ src/components/Buttons/ButtonBottom.js
@@ -0,0 +1,29 @@
+import { StyleSheet, Pressable } from 'react-native';
+import React from 'react';
+
+import PretendardedText from '../CustomComponent/PretendardedText';
+
+export const ButtonBottom = (props) => {
+  return (
+    <Pressable
+      style={styles.nextButton}
+      onPress={props.action}>
+      <PretendardedText style={styles.nextButtonText}>{props.text}</PretendardedText>
+    </Pressable>
+  );
+};
+
+const styles = StyleSheet.create({
+  nextButton: {
+    flex: 1,
+    backgroundColor: '#3CE3AC',
+    width: '100%',
+    height: 87,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  nextButtonText: {
+    fontWeight: 700,
+    fontSize: 20,
+  },
+});
