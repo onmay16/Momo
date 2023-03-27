1fb0bd36c9d68525b2cf4cf6c1d1f3c7c677b780
KangHyi Lee <corea22222@gmail.com>
2023-03-27 20:13:39 +0900

    MOMO-28 feat: apply redux state (6)
    Step contents 를 component 로 분리

--- src/components/tutorials/BottomButtonComponent.js
+++ src/components/tutorials/BottomButtonComponent.js
@@ -1,12 +1,34 @@
 import React from 'react';
 import { View, Text, TouchableOpacity } from 'react-native';
-import {useSelector} from 'react-redux';
+import {useSelector, useDispatch} from 'react-redux';
+import { Step } from '../../utils/tutorials/Step';
+import { 
+    setStep,
+} from '../../redux/reducerSlices/tutorialSlice';
 
 const BottomButtonComponent = props => {
+    const dispatch = useDispatch();
+
     const enableBottomBtn = useSelector((state) => state.tutorial.enableBottomBtn);
+    const step = useSelector((state) => state.tutorial.step);
+
+    function setStepfun(value) {
+        dispatch(setStep({
+            step: value,
+        }));
+    }
+
+    function clickBottomButton() {
+        if (step === Step.STEP_ONE){
+            setStepfun(Step.STEP_TWO);
+        }
+        else if(step === Step.STEP_TWO){
+
+        }
+    }
 
     return (
-        <TouchableOpacity disabled={!enableBottomBtn} style={{flex: 1, backgroundColor: "#3CE3AC", justifyContent:"center", opacity:enableBottomBtn ? 1 : 0}} onPress={() => alert("Bottom Button OnClick!")}>
+        <TouchableOpacity disabled={!enableBottomBtn} style={{flex: 1, backgroundColor: "#3CE3AC", justifyContent:"center", opacity:enableBottomBtn ? 1 : 0}} onPress={clickBottomButton}>
             <Text style={{textAlign:"center", color:"black", fontSize:20, fontWeight: 700}}>다음</Text>
         </TouchableOpacity>
     )
