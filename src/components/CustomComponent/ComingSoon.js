27d79540c21cf81c69147a9a6d0f481d0bcc5136
onmay16 <onmay16@gmail.com>
2023-03-27 21:43:55 -0500

    MOMO-42: add coming soon component

---
+++ src/components/CustomComponent/ComingSoon.js
@@ -0,0 +1,28 @@
+import { StyleSheet, View } from 'react-native';
+import React from 'react';
+
+import PretendardedText from './PretendardedText';
+
+export const ComingSoon = () => {
+  return (
+    <View style={styles.comingSoon}>
+      <PretendardedText style={styles.comingSoonText}>COMING SOON</PretendardedText>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  comingSoon: {
+    backgroundColor: '#D7E0FF',
+    borderRadius: 4,
+    height: 14,
+    width: 65,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  comingSoonText: {
+    color: '#6184FF',
+    fontWeight: 700,
+    fontSize: 7,
+  },
+});
