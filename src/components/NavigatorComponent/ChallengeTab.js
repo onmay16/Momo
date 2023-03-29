6c81910ca65ea5367b0491e198ee0906fe1af3b2
onmay16 <onmay16@gmail.com>
2023-03-28 22:03:51 -0500

    issue-17: update navigation tab

---
+++ src/components/NavigatorComponent/ChallengeTab.js
@@ -0,0 +1,31 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+import Challenge from '../../assets/icons/light/challenge.svg';
+import ChallengeFocused from '../../assets/icons/light/challengeFocused.svg';
+
+export const ChallengeTab = (props) => {
+  return (
+    <View style={styles.container}>
+      {props.focused ? <ChallengeFocused/> : <Challenge/>}
+      <Text style={[styles.text, customStyles(props.focused).text]}>챌린지</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  text: {
+    fontSize: 10,
+    marginTop: 5,
+  },
+});
+
+const customStyles = (focused) => StyleSheet.create({
+  text: {
+    fontWeight: focused ? 700 : 500,
+    color: focused ? '#3CE3AC' : '#B3B3B3',
+  },
+});
