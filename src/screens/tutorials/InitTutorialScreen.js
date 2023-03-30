7f80c4575dea0932140436ea367d9bfcdd7b1d17
KangHyi Lee <corea22222@gmail.com>
2023-03-30 22:46:14 +0900

    MOMO-28 feat: apply redux state (7)
    ANIMATION_TUTORIAL, END_TUTORIAL  애니메이션 완료

--- src/screens/tutorials/InitTutorialScreen.js
+++ src/screens/tutorials/InitTutorialScreen.js
@@ -14,6 +14,8 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import TutorialMomoImage from '../../assets/images/TutorialMomo.png';
 
+import TutorialMomoImage from '../../assets/images/TutorialMomo.png';
+
 import { 
   useBackgroundImg,
   setStep,
