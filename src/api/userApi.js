48cd2e3df3c0e8e75f980b7519664810411da0a4
onmay16 <onmay16@gmail.com>
2023-04-08 20:22:40 -0500

    issue-17: #17 update main screen API

--- src/api/userApi.js
+++ src/api/userApi.js
@@ -3,6 +3,18 @@ import { FIRESTORE_API_URL, PROJECT_ID } from '@env';
 
 import { getAuthToken } from '../utils/utils';
 
+export const getUserBasic = async () => {
+  try {
+    const response = await axios.get(
+      // TODO: replace 'user1' with token
+      `${FIRESTORE_API_URL}${PROJECT_ID}/databases/(default)/documents/User_Collection/user1`
+    );
+    return response;
+  } catch (error) {
+    return error;
+  }
+};
+
 export const getUserRoutine = async () => {
   try {
     const response = await axios.get(
--- src/redux/reducerSlices/userRoutineSlice.js
+++ src/redux/reducerSlices/userRoutineSlice.js
@@ -1,5 +1,12 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
-import { getUserRoutine } from '../../api/getUserRoutineApi';
+import { getUserRoutine } from '../../api/userApi';
+
+const initialState = {
+  userRoutineActionList: null,
+  isLoading: false,
+  error: null,
+  remainingTime: 0,
+};
 
 export const fetchUserRoutine = createAsyncThunk(
   'userRoutineSlice/fetchUserRoutine',
@@ -9,60 +16,73 @@ export const fetchUserRoutine = createAsyncThunk(
   }
 );
 
-const initialState = {
-  userRoutineActionList: null,
-  isLoading: false,
-  error: null,
-  status: '',
-};
+function extractRoutineData(payload) {
+  const data = payload.documents;
+  let userRoutineActionList = [];
+  for (let i = 0; i < data.length; i++) {
+    const routine = data[i];
+    const id = routine.name.match(/(r\w+)/g).at(-1);
+    const fields = routine.fields;
+    const duration = fields.duration.integerValue;
+    // TODO: remove unused vars if they're not needed
+    const emoji = fields.emoji.stringValue;
+    const category = fields.category.stringValue;
+    const routineName = fields.routine_name.stringValue;
+    const difficulty = fields.difficulty.integerValue;
+    const activeDay = fields.active_day.integerValue;
+    const streak = fields.streak.integerValue;
+    const routineObj = {
+      id: id,
+      name: routineName,
+      category: category,
+      complete: false,
+      duration: parseInt(duration),
+      difficulty: parseInt(difficulty),
+      streak: parseInt(streak),
+      activeDay: parseInt(activeDay),
+    };
+    userRoutineActionList.push(routineObj);
+  }
+  return userRoutineActionList;
+}
+
+function calculateDuration(array) {
+  const total = array.reduce((accumulator, object) => {
+    if (!object.complete) {
+      return accumulator + object.duration;
+    }
+    return accumulator;
+  }, 0);
+  return total;
+}
 
 export const userRoutineSlice = createSlice({
   name: 'userRoutineSlice',
   initialState,
-  reducers: {},
+  reducers: {
+    updateCompleteStatus: (state, action) => {
+      const index = state.userRoutineActionList.findIndex(routine => routine.id === action.payload);
+      const newArray = [...state.userRoutineActionList];
+
+      newArray[index].complete = !newArray[index].complete;
+    },
+  },
   extraReducers: (builder) => {
     builder
       .addCase(fetchUserRoutine.pending, (state) => {
-        state.status = 'pending';
         state.isLoading = true;
       })
       .addCase(fetchUserRoutine.fulfilled, (state, action) => {
-        state.status = 'fulfilled';
         state.isLoading = false;
-        // state.userRoutineActionList = action.payload;
-
-        const data = action.payload.documents;
-        let userRoutineActionList = [];
-        for (let i = 0; i < data.length; i++) {
-          const routine = data[i];
-          const id = routine.name.match(/(r\w+)/g).at(-1);
-          const fields = routine.fields;
-          const duration = fields.duration.integerValue;
-          // TODO: remove unused vars if they're not needed
-          const emoji = fields.emoji.stringValue;
-          const category = fields.category.stringValue;
-          const routineName = fields.routine_name.stringValue;
-          const difficulty = fields.difficulty.integerValue;
-          const activeDay = fields.active_day.integerValue;
-          const streak = fields.streak.integerValue;
-          const routineObj = {
-            id: id,
-            name: routineName,
-            category: category,
-            complete: false,
-            duration: duration,
-          };
-          userRoutineActionList.push(routineObj);
-        }
-        state.userRoutineActionList = userRoutineActionList;
+        state.userRoutineActionList = extractRoutineData(action.payload);
+        state.remainingTime = calculateDuration(state.userRoutineActionList);
       })
       .addCase(fetchUserRoutine.rejected, (state, action) => {
-        state.status = 'rejected';
         state.isLoading = false;
         state.error = action.error.message;
       });
   },
 });
 
-export const { } = userRoutineSlice.actions;
+export const { updateCompleteStatus } = userRoutineSlice.actions;
 export default userRoutineSlice.reducer;
