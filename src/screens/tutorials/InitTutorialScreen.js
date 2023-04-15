a20c359ea56c7dedf1eb0828c2c180feea2a9143
KangHyi Lee <corea22222@gmail.com>
2023-04-15 16:41:43 +0900

    MOMO-28 feat: apply redux state (10)
    컴포넌트 export 방식 변경
    기존: const {컴포넌트 명}
    변경: export const {컴포넌트 명}

--- src/screens/tutorials/InitTutorialScreen.js
+++ src/screens/tutorials/InitTutorialScreen.js
@@ -3,11 +3,6 @@ import {View, Animated, Text, Image} from 'react-native';
 import { TextContent } from '../../components/tutorials/TextContent';
 import PretendardedText from '../../components/CustomComponent/PretendardedText';
 
-import { Step } from '../../utils/tutorials/Step';
-import { useDispatch, useSelector } from 'react-redux';
-
-import TutorialMomoImage from '../../assets/images/TutorialMomo.png';
-
 import TutorialMomoImage from '../../assets/images/TutorialMomo.png';
 import { Step } from '../../utils/tutorials/Step';
 import { useDispatch, useSelector } from 'react-redux';
