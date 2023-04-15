6293eba452718e9bdbcf4936d24d699a0ab563a9
KangHyi Lee <corea22222@gmail.com>
2023-04-15 21:58:51 +0900

    MOMO-28 feat: apply redux state (14)
    튜토리얼의 BottomButton 을 common component 로 교체

--- src/screens/tutorials/MainTutorialScreen.js
+++ src/screens/tutorials/MainTutorialScreen.js
@@ -1,4 +1,4 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import {View, SafeAreaView, Animated} from 'react-native';
 import BackgroundImgComponent from '../../components/tutorials/BackgroundImgComponent';
 import TutorialHeader from '../../components/tutorials/TutorialHeader';
