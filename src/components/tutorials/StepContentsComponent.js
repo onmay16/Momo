fa7eb89f54f3df5aa28d6b38091069039d9fee67
KangHyi Lee <corea22222@gmail.com>
2023-04-16 21:34:47 +0900

    MOMO-28 feat: apply redux state (16)
    StepContents 글자색 반영

--- src/components/tutorials/StepContentsComponent.js
+++ src/components/tutorials/StepContentsComponent.js
@@ -6,11 +6,12 @@ import PretendardedText from '../../components/CustomComponent/PretendardedText'
 export const StepContentsComponent = props => {
     const stepNumber = useSelector((state) => state.tutorial.stepNumber);
     const stepText = useSelector((state) => state.tutorial.stepText);
+    const enableBackgroundImg = useSelector((state) => state.tutorial.enableBackgroundImg);
 
     return (
         <View style={{height: 70, justifyContent:'flex-start'}}>
             <PretendardedText style={{color:"#3CE3AC", fontSize: 17, fontWeight: 700}}>Step {stepNumber}.</PretendardedText>
-            <PretendardedText style={{color:props.textColor, fontSize: 17, fontWeight: 500}}>{stepText}</PretendardedText>
+            <PretendardedText style={{color:enableBackgroundImg ? "white" : "black", fontSize: 17, fontWeight: 500}}>{stepText}</PretendardedText>
         </View>
     )
 }
