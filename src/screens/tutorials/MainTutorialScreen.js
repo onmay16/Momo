042da52fa47aa3ec333f0ad40b544087c5e1aa36
KangHyi Lee <corea22222@gmail.com>
2023-04-16 21:34:47 +0900

    MOMO-28 feat: apply redux state (16)
    StepContents 글자색 반영

--- src/screens/tutorials/MainTutorialScreen.js
+++ src/screens/tutorials/MainTutorialScreen.js
@@ -6,6 +6,7 @@ import { InitTutorialScreen } from './InitTutorialScreen';
 import { TimePickerScreen } from './TimePickerScreen';
 import { ButtonBottom } from '../../components/Buttons/ButtonBottom';
 import { Step } from '../../utils/tutorials/Step';
+import { RoutineAddList } from '../../components/RoutineAddList/RoutineAddListModal';
 
 import { useDispatch, useSelector } from 'react-redux';
 import { 
