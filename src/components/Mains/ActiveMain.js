bae9d89a8282ef6f269ca85bac8dd32cbfc0dbdb
onmay16 <onmay16@gmail.com>
2023-04-17 22:52:38 -0500

    issue-17: #17 point animation

--- src/components/Mains/ActiveMain.js
+++ src/components/Mains/ActiveMain.js
@@ -1,10 +1,11 @@
 import { StyleSheet, View, Animated, Dimensions, Platform, ScrollView } from 'react-native';
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as Progress from 'react-native-progress';
 import { PretendardedText } from '../CustomComponent/PretendardedText';
-import { useDispatch, useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
 
 import Action from '../Action';
+import { Point } from '../CustomComponent/Point';
 
 //TO-DO: replace momo.png with sleeping momo gif
 import Dust from '../../assets/character/1_dust.svg';
@@ -15,24 +16,63 @@ export const ActiveMain = () => {
     const userState = useSelector(state => state.user);
     const userRoutineState = useSelector(state => state.userRoutineSlice);
 
+    const [animatedPoint, setAnimatedPoint] = useState(0);
+
     const opacity = useRef(new Animated.Value(0)).current;
-    useEffect(() => {
-        if (userState.momoActivated) {
-            Animated.timing(opacity, {
-                toValue: 1,
+    const pointOpacity = useRef(new Animated.Value(0)).current;
+    const pointY = useRef(new Animated.Value(100)).current;
+
+    function mainFadeIn() {
+        Animated.timing(opacity, {
+            toValue: 1,
+            useNativeDriver: true,
+        }).start();
+    }
+
+    function pointAnimation() {
+        Animated.timing(pointOpacity, {
+            toValue: 1,
+            duration: 1000,
+            useNativeDriver: true,
+        }).start();
+        Animated.timing(pointY, {
+            toValue: 0,
+            duration: 1000,
+            useNativeDriver: true,
+        }).start();
+        setTimeout(() => {
+            Animated.timing(pointOpacity, {
+                toValue: 0,
+                duration: 1000,
                 useNativeDriver: true,
             }).start();
+        }, 1000);
+        setTimeout(() => {
+            Animated.timing(pointY, {
+                toValue: 100,
+                duration: 1000,
+                useNativeDriver: true,
+            }).start();
+        }, 2000);
+    }
+
+    useEffect(() => {
+        if (userState.momoActivated) {
+            mainFadeIn();
         }
     }, []);
 
     return (
         <Animated.View style={[styles.container, { opacity: opacity }]}>
             <View style={styles.progressArea}>
+                <Animated.View style={[{ opacity: pointOpacity }, { transform: [{ translateY: pointY }] }]}>
+                    <Point amount={animatedPoint}/>
+                </Animated.View>
                 <View style={styles.momo}>
                     <Dust/>
                 </View>
                 <View style={styles.currentPointContainer}>
-                    <PretendardedText style={styles.currentPoint}>{userState.currentPoint} </PretendardedText>
+                    <PretendardedText style={styles.currentPoint}>{Math.floor(userState.currentPoint / userState.requiredPointToNextLevel * 100)}% </PretendardedText>
                     <CurrentPointFire />
                 </View>
                 <View style={{ justifyContent: 'center' }}>
@@ -61,7 +101,7 @@ export const ActiveMain = () => {
             <View style={styles.actionsList}>
                 <ScrollView style={{ width: '100%' }} showsVerticalScrollIndicator={false}>
                     {userRoutineState.userRoutineActionList.map((action) => (
-                        <Action id={action.id}/>
+                        <Action id={action.id} setAnimatedPoint={setAnimatedPoint} pointAnimation={pointAnimation}/>
                     ))}
                 </ScrollView>
             </View>
@@ -74,7 +114,7 @@ const styles = StyleSheet.create({
     progressArea: { flex: 1, marginRight: 39, marginLeft: 39, marginBottom: 29 },
     momo: { flex: this.momoActivated ? (Platform.OS === 'ios' ? 1 : 0.5) : (Platform.OS === 'ios' ? 4 : 3), justifyContent: 'center', alignItems: 'center' },
     currentPointContainer: { flexDirection: 'row', marginBottom: 3, alignItems: 'center' },
-    currentPoint: { fontWeight: 700, fontSize: 14, color: '#FF6056', marginRight: 2 },
+    currentPoint: { fontWeight: 700, fontSize: 14, color: '#3CE3AC', marginRight: 2 },
     remainingPointContainer: { flexDirection: 'row', marginTop: 3, alignItems: 'center', justifyContent: 'flex-end' },
     todayRoutineContainer: { flexDirection: 'row', justifyContent: 'space-between', alignItems: 'flex-start', marginBottom: 14 },
     todayRoutine: { fontWeight: 600, fontSize: 16, color: '#4C4C4C' },
