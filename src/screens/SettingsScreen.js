1e20588e7f5d90f116bc8ae4905b7e9ec2db4183
onmay16 <onmay16@gmail.com>
2023-04-30 03:10:38 -0500

    MOMO-19: add safe area view

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, View, Switch } from 'react-native';
+import { StyleSheet, View, Switch, SafeAreaView } from 'react-native';
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 
@@ -24,25 +24,27 @@ export const SettingsScreen = () => {
     }
 
     return (
-        <View style={{ flex: 1, alignItems: 'center' }}>
-            <PretendardedText style={styles.header}>환경설정</PretendardedText>
-            <View style={styles.buttons}>
-                <ButtonLarge type={'default'} text={'66팀 이야기'} icon={false} action={test}/>
-                <ButtonLarge style={styles.button}  type={'dark'} text={'모모스토리'} icon={false} action={test}/>
-                <ButtonLarge style={styles.button}  type={'disabled'} text={'즉각 피드백'} icon={false} action={test}/>
-                <ButtonLarge style={styles.button}  type={'light'} text={'출시 알림 받기'} icon={true} action={test}/>
-                <ButtonLarge style={styles.button}  type={'red'} text={'로그아웃'} icon={false} action={logout}/>
+        <View style={{ flex: 1, backgroundColor: '#F9F9F9'}}>
+            <SafeAreaView style={{ flex: 1, alignItems: 'center', width: '100%', }}>
+                <PretendardedText style={styles.header}>환경설정</PretendardedText>
+                <View style={styles.buttons}>
+                    <ButtonLarge type={'default'} text={'66팀 이야기'} icon={false} action={test} />
+                    <ButtonLarge style={styles.button} type={'dark'} text={'모모스토리'} icon={false} action={test} />
+                    <ButtonLarge style={styles.button} type={'disabled'} text={'즉각 피드백'} icon={false} action={test} />
+                    <ButtonLarge style={styles.button} type={'light'} text={'출시 알림 받기'} icon={true} action={test} />
+                    <ButtonLarge style={styles.button} type={'red'} text={'로그아웃'} icon={false} action={logout} />
                 <View style={styles.toggleRow}>
-                    <PretendardedText style={styles.toggleText}>사진으로 인증하기</PretendardedText>
-                    <Switch
-                        trackColor={{ false: '#595959', true: '#3CE3AC' }}
-                        thumbColor={isEnabled ? '#FFFFFF' : '#B3B3B3'}
-                        ios_backgroundColor="#3e3e3e"
-                        onValueChange={toggleSwitch}
-                        value={isEnabled}
-                    />
+                        <PretendardedText style={styles.toggleText}>사진으로 인증하기</PretendardedText>
+                        <Switch
+                            trackColor={{ false: '#595959', true: '#3CE3AC' }}
+                            thumbColor={isEnabled ? '#FFFFFF' : '#B3B3B3'}
+                            ios_backgroundColor="#3e3e3e"
+                            onValueChange={toggleSwitch}
+                            value={isEnabled}
+                        />
+                    </View>
                 </View>
-            </View>
+            </SafeAreaView>
         </View>
     );
 };
@@ -61,6 +63,7 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         flex: 0.85,
         justifyContent: 'space-between',
+        backgroundColor: '#F9F9F9',
     },
     toggleRow: {
         flexDirection: 'row',
