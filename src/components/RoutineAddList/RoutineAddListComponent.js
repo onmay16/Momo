18b0102933aeab279b50d3f67149576ff279d195
JDhyeok <jdhyeok97@gmail.com>
2023-05-01 16:03:52 +0900

    hotfix: chagne data type

--- src/components/RoutineAddList/RoutineAddListComponent.js
+++ src/components/RoutineAddList/RoutineAddListComponent.js
@@ -128,16 +128,16 @@ export const RoutineAddListComponent = (props) => {
            </ScrollView>
            <View style={{height:110, alignItems:'flex-end', justifyContent:'flex-start', position:'absolute', right:0, bottom:0, width:'100%'}} pointerEvents="none">
                 <View style={{flexDirection:'row', alignItems:'baseline', justifyContent:'flex-end'}}>
-                    <PretendardedText style={{fontWeight: 700, fontSize: 30, color:"#3CE3AC"}}>N</PretendardedText>
-                    <PretendardedText style={{fontWeight: 500, fontSize: 16, color:"#FFFFFF"}}>/28</PretendardedText>
+                    <PretendardedText style={{fontWeight: '700', fontSize: 30, color:"#3CE3AC"}}>N</PretendardedText>
+                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#FFFFFF"}}>/28</PretendardedText>
                 </View>
-                <PretendardedText style={{fontWeight: 500, fontSize: 16, color:"#D9D9D9", textAlign:'right'}}>루틴을 마칠 시간까지</PretendardedText>
+                <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#D9D9D9", textAlign:'right'}}>루틴을 마칠 시간까지</PretendardedText>
                 <View style={{flexDirection:'row', justifyContent:'flex-end'}}>
-                    <PretendardedText style={{fontWeight: 500, fontSize: 16, color:"#3CE3AC"}}>NN분 더 </PretendardedText>
-                    <PretendardedText style={{fontWeight: 500, fontSize: 16, color:"#D9D9D9"}}>여유있어요.</PretendardedText>
+                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#3CE3AC"}}>NN분 더 </PretendardedText>
+                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#D9D9D9"}}>여유있어요.</PretendardedText>
                 </View>
                 <View style={{marginTop:5}}>
-                    <PretendardedText style={{fontWeight: 500, fontSize: 12, color:"#B3B3B3", textAlign:'right'}}>※ 루틴별 예상 소요시간은 나중에 수정할 수 있어요.</PretendardedText>
+                    <PretendardedText style={{fontWeight: '500', fontSize: 12, color:"#B3B3B3", textAlign:'right'}}>※ 루틴별 예상 소요시간은 나중에 수정할 수 있어요.</PretendardedText>
                 </View>
             </View>
         </SafeAreaView>
@@ -150,7 +150,7 @@ const styles = StyleSheet.create({
         marginTop: 20,
     },
     listTitle: {
-        fontWeight: 700,
+        fontWeight: '700',
         fontSize: 12,
         marginBottom: '4%',
         color: '#4C4C4C',
