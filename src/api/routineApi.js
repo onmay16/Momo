584593bd70588b9e3a4a0aeaf9c9e01580d419ac
JDhyeok <jdhyeok97@gmail.com>
2023-05-05 10:28:09 +0900

    MOMO-41 add delete routine api

--- src/api/routineApi.js
+++ src/api/routineApi.js
@@ -2,13 +2,23 @@ import axios from 'axios';
 import {FIRESTORE_API_URL, PROJECT_ID} from '@env';
 
 export const getRoutine = async () => {
-    const response = await axios.get(
-        `${FIRESTORE_API_URL}${PROJECT_ID}/databases/(default)/documents/Routine_Collection/`
-    ).then(res => {
-      return res;
-    }).catch(e => {
-      return e;
-    });
-    
-    return response.data;
+  const response = await axios.get(
+      `${FIRESTORE_API_URL}${PROJECT_ID}/databases/(default)/documents/Routine_Collection/`
+  ).then(res => {
+    return res;
+  }).catch(e => {
+    return e;
+  });
+  
+  return response.data;
+};
+
+export const deleteRoutineApi = async (routineId) => {
+  await axios.delete(
+    `${FIRESTORE_API_URL}${PROJECT_ID}/databases/(default)/documents/User_Collection/user1/Routine_Collection/${routineId}`
+  ).then(res => {
+    return res;
+  }).catch(e => {
+    return e;
+  });
 };
