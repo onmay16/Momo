cdcd2b0cb242d96dba63ffd2357e41a60cb89de5
JDhyeok <jdhyeok97@gmail.com>
2023-05-10 23:20:01 +0900

    MOMO-74 fix signUp anonymously api

--- src/api/userAuthApi.js
+++ src/api/userAuthApi.js
@@ -3,33 +3,65 @@ import {AUTH_API_URL, API_KEY, FIRESTORE_API_URL, PROJECT_ID} from '@env';
 
 import {storeAuthToken} from '../utils/utils';
 
-const signInAnonymously = async (callBack) => {
-  try{
-    const response = await axios.post(
-      `${AUTH_API_URL}accounts:signUp?key=${API_KEY}`,
-      {
-        returnSecureToken: true,
-      },
-    ).then(response => {
-      return response
-    }).catch( e => {
-      console.log(e);
-    });    
-    await userDocumnetSetup(response.data.idToken);
-    await storeAuthToken(response.data.idToken).then(()=>{callBack(response.data.idToken)});
-  } catch (error) {
-    console.error(error);
-  }
+export const signUpAnonymously = async (callBack) => {
+
+  const response = await axios.post(
+    `${AUTH_API_URL}accounts:signUp?key=${API_KEY}`,
+    {
+      'returnSecureToken': true,
+    },
+  ).then(response => {
+    return response.data
+  }).catch( e => {
+    console.log(e);
+  });    
+  await storeAuthToken(response.localId).then(()=>{callBack(response.localId)});
+  return response.localId;
 };
 
-const userDocumnetSetup = async (token) => {
-  try{
-    await axios.post(
-      `${FIRESTORE_API_URL}${PROJECT_ID}/databases/(default)/documents/User_Collection?documentId=${token}`
-    )
-  } catch (error) {
-    console.error(error);
-  }
-}
+// 추후 사용 예정
+export const signInWithToken = async (token) => {
+  const uuid = await axios.post(
+    `${AUTH_API_URL}accounts:signInWithCustomToken?key=${API_KEY}`,
+    {
+      'token': token,
+      'returnSecureToken': true,
+    }
+  ).then(response => {
+    console.log(response);
+    return response
+  }).catch(e => {
+    console.log(e);
+  })
+  
+  return uuid;
+} 
 
-export {signInAnonymously};
+export const userDocumnetSetup = async (uuid) => {
+  const data = {
+    fields: {
+      email: { stringValue: 'alpha@user.com' },
+      is_activated: { booleanValue: false },
+      momo_exp: { integerValue: 0 },
+      password: { stringValue: 'password' },
+      routine_complete_time: { timestampValue: '2023-03-22T12:30:00.834Z' },
+      streak: { integerValue: 0 },
+      user_name: { stringValue: 'alpha 사용자' },
+      wake_up_time: { timestampValue: '2023-03-22T12:30:00.834Z' }, 
+    }
+  };
+  
+  await axios.patch(
+    `${FIRESTORE_API_URL}${PROJECT_ID}/databases/(default)/documents/User_Collection/${uuid}`,
+      data,
+      {
+        headers: {
+          'Content-type': 'application/json',
+        },
+      }
+    ).then(response => {
+      return response
+    }).catch(e => {
+      console.log(e);
+    })
+}
