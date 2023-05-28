523cdf57bcd34baac4790d9a88705661091f7cad
JDhyeok <jdhyeok97@gmail.com>
2023-05-28 16:42:45 +0900

    hotfix: add green star at routine add modal

--- src/components/RoutineAddList/RoutineAddListComponent.js
+++ src/components/RoutineAddList/RoutineAddListComponent.js
@@ -4,6 +4,7 @@ import { CategoryRoutineList } from './CategoryRoutineList';
 import { PretendardedText } from '../CustomComponent/PretendardedText';
 
 import { getRoutine } from '../../api/routineApi';
+import GreenStar from '../../assets/images/green_star.svg'
 
 export const RoutineAddListComponent = (props) => {
     const [healthRoutines, setHealthRoutines] = useState([]);
@@ -55,19 +56,25 @@ export const RoutineAddListComponent = (props) => {
                 <CategoryRoutineList category="성장" routines={selfHelpRoutines} isTutorial={props.isTutorial} />
                 <CategoryRoutineList category="생활" routines={livingRoutines} isTutorial={props.isTutorial} />
            </ScrollView>
-           <View style={{height:110, alignItems:'flex-end', justifyContent:'flex-start', position:'absolute', right:0, bottom:0, width:'100%'}} pointerEvents="none">
-                <View style={{flexDirection:'row', alignItems:'baseline', justifyContent:'flex-end'}}>
+           <View style={{height: props.isTutorial ? 110 : 90, alignItems:'flex-end', justifyContent:'flex-start', position:'absolute', right:0, bottom:0, width:'100%'}} pointerEvents="none">
+                <View style={{flexDirection:'row', alignItems: 'center' , justifyContent:'flex-end' }}>
                     <PretendardedText style={{fontWeight: '700', fontSize: 30, color:"#3CE3AC"}}>N</PretendardedText>
-                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#FFFFFF"}}>/28</PretendardedText>
+                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color: props.isTutorial ?  "#FFFFFF" : "#22222"}}>/28 </PretendardedText>
+										<GreenStar/>
                 </View>
-                <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#D9D9D9", textAlign:'right'}}>루틴을 마칠 시간까지</PretendardedText>
+                <PretendardedText style={{fontWeight: '500', fontSize: 16, color: props.isTutorial ? "#D9D9D9" : "#4C4C4C", textAlign:'right'}}>루틴을 마칠 시간까지</PretendardedText>
                 <View style={{flexDirection:'row', justifyContent:'flex-end'}}>
-                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#3CE3AC"}}>NN분 더 </PretendardedText>
-                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color:"#D9D9D9"}}>여유있어요.</PretendardedText>
+                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color: "#3CE3AC" }}>NN분 더 </PretendardedText>
+                    <PretendardedText style={{fontWeight: '500', fontSize: 16, color: props.isTutorial ? "#D9D9D9" : "#4C4C4C" }}>여유있어요.</PretendardedText>
                 </View>
-                <View style={{marginTop:5}}>
-                    <PretendardedText style={{fontWeight: '500', fontSize: 12, color:"#B3B3B3", textAlign:'right'}}>※ 루틴별 예상 소요시간은 나중에 수정할 수 있어요.</PretendardedText>
-                </View>
+                {
+									props.isTutorial ?
+										<View style={{marginTop:5}}>
+											<PretendardedText style={{fontWeight: '500', fontSize: 12, color:"#B3B3B3", textAlign:'right'}}>※ 루틴별 예상 소요시간은 나중에 수정할 수 있어요.</PretendardedText>
+										</View>
+										:
+										<></>
+                }
             </View>
         </SafeAreaView>
     );
