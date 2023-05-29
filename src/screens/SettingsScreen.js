3dbf850a9cc15c392543c8ef52fd825baa6292a6
JDhyeok <jdhyeok97@gmail.com>
2023-05-29 14:45:37 +0900

    MOMO-90 add version info button

--- src/screens/SettingsScreen.js
+++ src/screens/SettingsScreen.js
@@ -20,6 +20,7 @@ export const SettingsScreen = () => {
                     <ButtonLarge type={'default'} text={'66팀 이야기'} icon={false} action={test} />
                     <ButtonLarge type={'default'} text={'모모스토리'} icon={false} action={test} />
                     <ButtonLarge type={'default'} text={'즉각 피드백'} icon={false} action={test} />
+                    <ButtonLarge type={'default'} text={'버전 정보'} icon={false} action={test} />
                     <ButtonLarge type={'default'} text={'출시 알림 받기'} icon={true} action={test} />
                     {/* <View style={styles.toggleRow}>
                         <PretendardedText style={styles.toggleText}>사진으로 인증하기</PretendardedText>
