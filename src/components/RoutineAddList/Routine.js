6ae95da4b2c20ee52845ee7316d6c771bfe09d1c
JDhyeok <jdhyeok97@gmail.com>
2023-05-29 13:38:10 +0900

    MOMO-86 add multiple click available feature

--- src/components/RoutineAddList/Routine.js
+++ src/components/RoutineAddList/Routine.js
@@ -3,24 +3,41 @@ import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 import { PretendardedText } from '../CustomComponent/PretendardedText';
-import { toggleClick } from '../../redux/reducerSlices/routineSlice';
+import { toggleClick, addRoutine, removeRoutine } from '../../redux/reducerSlices/routineSlice';
 
 import Difficulty from '../../assets/images/difficultyStar.svg';
 
 export const Routine = (props) => {
   const dispatch = useDispatch();
   const clickedRoutineId = useSelector((state) => state.routineSlice.clickedRoutineId);
+  const clickedRoutineList = useSelector((state) => state.routineSlice.clickedRoutineList);
 
   const toggleButton = () => {
     const {id, category, name, emoji, duration, difficulty} = props;
-    dispatch(toggleClick({id, category, name, emoji, duration, difficulty}));
-    console.log(props.isTutorial)
+    const routineIdx = clickedRoutineList.findIndex(routine => routine.id === id);
+    if (props.isTutorial === true) {
+      if (routineIdx !== -1) {
+        dispatch(removeRoutine(id))
+      } else {
+        dispatch(addRoutine({id, category, name, emoji, duration, difficulty}))
+      }
+    } else {
+      dispatch(toggleClick({id, category, name, emoji, duration, difficulty}));
+    }
+  }
+
+  const changeClickedRoutineBackgroundColor = () => {
+    if (props.isTutorial === true){
+      return clickedRoutineList.some(routine => routine.id === props.id) ? '#3CE3AC' : 'transparent';
+    } else {
+      return clickedRoutineId === props.id ? '#3CE3AC' : 'transparent'; 
+    }
   }
 
   return (
       <View style={styles.container}>
         <TouchableOpacity onPress={toggleButton}>
-          <View style={[styles.button, {backgroundColor: clickedRoutineId === props.id ? '#3CE3AC' : 'transparent'}]}>
+          <View style={[styles.button, {backgroundColor: changeClickedRoutineBackgroundColor()}]}>
             <PretendardedText style={props.isTutorial ? tutorialStyle.text : styles.text }>{props.emoji}  {props.name} (+{props.duration}분) </PretendardedText>
               <View style={{ justifyContent: 'center' }}>
                 <Difficulty />
@@ -42,7 +59,6 @@ const styles = StyleSheet.create({
   },
   button: {
     flexDirection: 'row',
-    // backgroundColor: '#3CE3AC',
     alignItems: 'center',
     height:25,
     paddingHorizontal: 5,
