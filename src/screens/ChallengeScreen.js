0ccd24a9ec7f90c472d2562e0fc1732f173a4b1c
KangHyi Lee <corea22222@gmail.com>
2023-06-11 22:30:11 +0900

    MOMO-91: add challenge screen

--- src/screens/ChallengeScreen.js
+++ src/screens/ChallengeScreen.js
@@ -1,14 +1,30 @@
-import { Text, View, StyleSheet } from 'react-native';
+import { Image, View, StyleSheet, Dimensions } from 'react-native';
 import React from 'react';
 
+import Background from '../assets/images/challenge_background.png';
+import ComingSoonImage from '../assets/images/wip.svg';
+
 const ChallengeScreen = () => {
+    var {height, width} = Dimensions.get('window');
     return (
-        <View>
-            <Text>ChallengeScreen</Text>
+        <View style={styles.container}>
+            <Image source={Background} style={{height: height, width: width, resizeMode: 'stretch'}}/>
+            <ComingSoonImage style={styles.coomingsoonContainer}/>
         </View>
     );
 };
 
-export default ChallengeScreen;
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+        height: '100%',
+        width: '100%'
+    },
+    coomingsoonContainer:{
+        position:'absolute'
+    }
+});
 
-const styles = StyleSheet.create({});
+export default ChallengeScreen;
